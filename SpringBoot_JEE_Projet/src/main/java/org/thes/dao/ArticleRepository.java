b37af618a2201e8bdd7f15edf5f871adfaea3e95
Raphael-Mssn <72137562+Raphael-Mssn@users.noreply.github.com>
2021-03-14 15:35:19 +0100

    Modifications pour faire fonctionner articles.html

--- SpringBoot_JEE_Projet/src/main/java/org/thes/dao/ArticleRepository.java
+++ SpringBoot_JEE_Projet/src/main/java/org/thes/dao/ArticleRepository.java
@@ -4,10 +4,16 @@ import org.thes.entities.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleRepository extends JpaRepository<Article, Long>{
 	
-	public Page<Article> findByDesignationContains(String mc, Pageable pageable);
+	public Page<Article> findByLibelleContains(String mc, Pageable pageable);
+	
+	@Query ("select a from Article a where a.libelle like :x and a.prixUnitaire>:y")
+	public Page<Article> chercher(
+			@Param("x")String mc,
+			@Param("y")double prixMin, Pageable pageable);
 
 }
