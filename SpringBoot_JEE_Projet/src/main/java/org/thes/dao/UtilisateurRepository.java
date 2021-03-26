f696788e56723469ae4b6a4ddfff013999b2023b
Camille ROUMIER <cr.roumier.camille@gmail.com>
2021-03-26 08:41:48 +0100

    Ajout d'une liste de favoris

--- SpringBoot_JEE_Projet/src/main/java/org/thes/dao/UtilisateurRepository.java
+++ SpringBoot_JEE_Projet/src/main/java/org/thes/dao/UtilisateurRepository.java
@@ -4,5 +4,7 @@ import org.thes.entities.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long>{
+	
+	
 
 }
